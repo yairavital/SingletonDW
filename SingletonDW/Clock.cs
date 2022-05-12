fdd55fdc6662eda21aaada3bea79c7141c7d07d2
yairavital <97466607+yairavital@users.noreply.github.com>
2022-05-12 22:16:19 +0300

    Add project files.

---
+++ SingletonDW/Clock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDW
+{
+    internal class Clock
+    {
+        private int hour;
+        private static Clock instance;
+        private static readonly object key = new object();
+        private Clock(int hour)
+        {
+            this.hour = hour;
+        }
+        public static Clock GetTime()
+        {
+            if (instance == null)
+            {
+                lock (key)
+                {
+                    if (instance == null)
+                    {
+                        Console.WriteLine("Enter your time");
+                        int hour = int.Parse(Console.ReadLine());
+                        instance = new Clock(hour);
+                    }
+
+                }
+
+            }
+
+            return instance;
+        }
+
+    }
+}
+
+ 
+
---
+++ SingletonDW/Program.cs
@@ -0,0 +1,6 @@
+﻿using SingletonDW;
+
+Clock clock = Clock.GetTime();
+Clock clock1 = Clock.GetTime();
+if (clock.Equals(clock1))
+    Console.WriteLine("The same instance");
